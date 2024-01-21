fb9c4f8d486f2763811d1b6ceb08e5949a6a07b7
gopendra113 <gopendra.singh@students.iiit.ac.in>
2024-01-21 10:19:46 +0530

    modified app.py

--- app.py
+++ app.py
@@ -5,70 +5,12 @@ import os
 app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 
-# Assuming you have a SQLite database file named 'your_database.db'
 DATABASE = 'research.db'
 
-
-# conn = sqlite3.connect('research.db')
-# cursor = conn.cursor()
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS users (
-#         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         name TEXT NOT NULL,
-#         username TEXT NOT NULL UNIQUE,
-#         password TEXT NOT NULL,
-#         email TEXT NOT NULL,
-#         lab_id INTEGER,
-#         FOREIGN KEY (lab_id) REFERENCES labs(lab_id)
-#     )
-# ''')
-
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS posts (
-#         post_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#         text_desc TEXT NOT NULL,
-#         username TEXT NOT NULL,
-#         file_name TEXT,
-#         type TEXT,
-#         FOREIGN KEY (username) REFERENCES users(username)
-#     )
-# ''')
-
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS labs (
-#         lab_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         lab_name TEXT NOT NULL
-#     )
-# ''')
-# conn.commit()
-# conn.close()
 
 @app.route('/',  methods=['POST','GET'])
 def index():
     return render_template('index.html')
-# @app.route('/')
-# def fetch_file_names():
-#     # Connect to the database
-#     conn = sqlite3.connect(DATABASE)
-#     cursor = conn.cursor()
-
-#     # Fetch file names sorted by time
-#     cursor.execute('''
-#         SELECT file_name FROM posts
-#         ORDER BY time DESC
-#     ''')
-
-#     # Fetch all results into a list
-#     result = cursor.fetchall()
-
-#     # Close the database connection
-#     conn.close()
-
-#     # Extract file names from the result
-#     file_names = [row[0] for row in result]
-
-#     return render_template('home.html', file_names=file_names)
 
 def get_UID():
     conn = sqlite3.connect('research.db')
@@ -84,9 +26,6 @@ def get_UID():
     if re==None:
         UID=1
     else:
-        # for x in user_id:
-        #     for y in x:
-        #         UID=y
         UID=re+1
 
     return UID
@@ -170,7 +109,6 @@ def home():
             print(username)
 
             file_names=fetch_file_names()
-            # return render_template('home.html')/
             return redirect("/Login")
 
         
@@ -178,71 +116,10 @@ def home():
 
 @app.route('/Signup')
 def Signup():
-        # conn = sqlite3.connect('research.db')
-        # cursor = conn.cursor()
-
-        # name = request.form['name']
-        # email = request.form['email']
-        # username = request.form['username']
-        # password = request.form['password']
-        # lab_id = request.form['lab_id']  # Note: Use request.form['lab_id'] instead of request.form('lab_id')
-        # lab_id=int(lab_id)
-
-        # d = cursor.execute("SELECT Username FROM users")
-        # for x in d:
-        #     for i in x:
-        #         if username == i:
-        #             flash('Username exists! Try a different one.')
-        #             messages = get_flashed_messages()
-        #             return render_template('Signup.html', messages=messages)
-
-        # UID=get_UID()
-        
-
-        # sql = "INSERT INTO users VALUES (%d,'%s', '%s', '%s', '%s',%d)" % (int(UID), name, username, password, email, int(lab_id))
-        # cursor.execute(sql)
-        # conn.commit()
-        # conn.close()
-        # print(username)
-
-        # file_names=fetch_file_names()
-
-        # # Pass lab_id as a parameter to 'home.html'
-        # return render_template('home.html', lab_id=lab_id,user_id=UID,file_names=file_names)
-
-    # Handle GET requests
     return render_template('Signup.html')
 
 @app.route('/Login')
 def Login():
-    # if request.method == 'POST':
-    #     username = request.form['username']
-    #     password = request.form['password']
-    #     conn = sqlite3.connect('research.db')
-    #     cursor = conn.cursor()
-
-    #     passo = cursor.execute('''SELECT Password FROM users WHERE username=? ''', (username,))
-
-    #     p = 0
-    #     for x in passo:
-    #         for y in x:
-    #             p = y
-        
-
-    #     if p != password or p == 0:
-    #         flash('Login Unsuccessful! Try again')
-    #         messages = get_flashed_messages()
-    #         return render_template('Login.html', messages=messages)
-    #     else:
-    #         # Add a return statement for the successful login
-    #         ul=cursor.execute('''SELECT user_id,lab_id FROM users WHERE username=? ''', (username,))
-    #         for x in ul:
-    #             UID,lab_id=x
-            
-    #         file_names=fetch_file_names()
-    #         return render_template('home.html',user_id=UID,lab_id=lab_id,file_names=file_names)
-    #     conn.close()
-    # Handle GET requests
     return render_template('Login.html')
 
 
@@ -312,13 +189,11 @@ def upload():
         conn = sqlite3.connect(DATABASE)
         cursor = conn.cursor()
 
-        # Insert a new row into the 'posts' table
         cursor.execute('''
             INSERT INTO posts (text_desc, username, file_name, type)
             VALUES (?, ?, ?, ?)
         ''', (text_desc, user_id, new_filename, type))
 
-        # Commit the changes and close the database connection
         conn.commit()
         conn.close()  
         
@@ -326,12 +201,8 @@ def upload():
 
 @app.route('/open_upload', methods=['POST'])
 def open_upload():
-    # Retrieve variables from the request
     user_id = request.form['user_id']
     lab_id = request.form['lab_id']
-    # variable2 = request.args.get('variable2', 'default_value2')
-
-    # Render post.html with variables
     return render_template('upload.html', user_id=user_id, lab_id=lab_id)
 
 
